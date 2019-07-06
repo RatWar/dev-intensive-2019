b7b84d628dd939ae954d44f149e321a8af18e92f
RatWar <aa960008>
2019-07-06 13:30:27 +0500

    hometask_2 complete-4

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,16 +1,45 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
-    fun parseFullName(fullName: String?): Pair<String?, String?> {
-        return when (fullName) {
-            "", " ", null -> Pair(null, null)
-            else -> {
-                val parts: List<String>? = fullName.split(" ")
-                val firstName = parts?.getOrNull(0)
-                val lastName = parts?.getOrNull(1)
-                firstName to lastName
-            }
+
+    private val translitMap = mapOf(
+        'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e", 'ж' to "zh", 'з' to "z",
+        'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m", 'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r",
+        'с' to "s", 'т' to "t", 'у' to "u", 'ф' to "f", 'х' to "h", 'ц' to "c", 'ч' to "ch", 'ш' to "sh", 'щ' to "sh",
+        'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya"
+    )
+//    fun parseFullName(fullName: String?): Pair<String?, String?> {
+//        return when (fullName) {
+//            "", " ", null -> Pair(null, null)
+//            else -> {
+//                val parts: List<String>? = fullName.split(" ")
+//                val firstName = parts?.getOrNull(0)
+//                val lastName = parts?.getOrNull(1)
+//                firstName to lastName
+//            }
+//        }
+//    }
+
+    private fun String.replaceAll(oldValue: String, newValue: String): String {
+        var result = this
+        while (result.contains(oldValue)) {
+            result = result.replace(oldValue, newValue)
         }
+        return result
+    }
+
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.trim()?.replaceAll("  ", " ")?.split(" ")
+
+        val firstName = parts?.notEmptyOrNullAt(0)
+        val lastName = parts?.notEmptyOrNullAt(1)
+
+        return firstName to lastName
+    }
+
+    private fun List<String>.notEmptyOrNullAt(index: Int) = getOrNull(index).let {
+        if ("" == it) null
+        else it
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
@@ -33,50 +62,24 @@ object Utils {
         }
     }
 
-    fun transliteration(payload: String, divider: String = " "): String {
-        val first = payload.substringBefore(" ").decapitalize()
-        val last = payload.substringAfter(" ").decapitalize()
-        return replace(first).capitalize() + divider + replace(last).capitalize()
-    }
+//    fun toInitials(firstName: String?, lastName: String?): String? = when {
+//        firstName.isNullOrBlank() && lastName.isNullOrBlank() -> null
+//        !firstName.isNullOrBlank() && lastName.isNullOrBlank() -> firstName[0].toUpperCase().toString()
+//        firstName.isNullOrBlank() && !lastName.isNullOrBlank() -> lastName[0].toUpperCase().toString()
+//        !firstName.isNullOrBlank() && !lastName.isNullOrBlank() -> firstName[0].toUpperCase() + lastName[0].toUpperCase().toString()
+//        else -> throw IllegalStateException("Incorrect state in 'when' expression")
+//    }
 
-    private fun replace(src: String) = run {
-        src.replace(Regex("[абвгдеёжзийклмнопрстуфхцчшщъыьэюя]")) {
-            when (it.value) {
-                "а" -> "a"
-                "б" -> "b"
-                "в" -> "v"
-                "г" -> "g"
-                "д" -> "d"
-                "е" -> "e"
-                "ё" -> "e"
-                "ж" -> "zh"
-                "з" -> "z"
-                "и" -> "i"
-                "й" -> "i"
-                "к" -> "k"
-                "л" -> "l"
-                "м" -> "m"
-                "н" -> "n"
-                "о" -> "o"
-                "п" -> "p"
-                "р" -> "r"
-                "с" -> "s"
-                "т" -> "t"
-                "у" -> "u"
-                "ф" -> "f"
-                "х" -> "h"
-                "ц" -> "c"
-                "ч" -> "ch"
-                "ш" -> "sh"
-                "щ" -> "sh'"
-                "ъ" -> ""
-                "ы" -> "i"
-                "ь" -> ""
-                "э" -> "e"
-                "ю" -> "yu"
-                "я" -> "ya"
-                else -> it.value
-            }
+    fun transliteration(payload: String, divider: String = " ") = buildString {
+        payload.forEach {
+            append(
+                when {
+                    it == ' ' -> divider
+                    it.isUpperCase() -> translitMap[it.toLowerCase()]?.capitalize() ?: it.toString()
+                    else -> translitMap[it] ?: it.toString()
+                }
+            )
         }
     }
+
 }
