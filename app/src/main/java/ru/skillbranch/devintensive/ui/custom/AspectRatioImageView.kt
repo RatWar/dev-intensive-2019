a7baa8746cffd34946a063993756eafc5592ae4b
RatWar <aa960008>
2019-07-30 21:01:29 +0500

    hometask_4 complete-3

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
@@ -26,7 +26,8 @@ class AspectRatioImageView @JvmOverloads constructor(
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val newHeight = (measuredWidth/aspectRatio).toInt()
+        val newHeight = (measuredWidth / aspectRatio).toInt()
         setMeasuredDimension(measuredWidth, newHeight)
     }
 }
+
